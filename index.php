2a4a692f32447f90e2368a90e8712309074306d2
FerBayley <fernando.bayley@gmail.com>
2015-04-05 03:51:54 -0300

    Modernizacion del index.php

---
+++ index.php
@@ -0,0 +1,221 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Arquinova Color - Revestimientos y pinturas</title>
+
+	<meta name="description" content="Trabajamos con las mejores marcas en pinturas y revestimeintos para que su proyecto arquitectonico se realice con la mejor calidad">
+
+	<!-- VIEWPORT -->
+	<meta name="viewport" content="width=device-width, user-scalable=no"/>
+	<!-- VIEWPORT -->
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<!-- CSS -->
+
+	<!-- JS -->
+	<script type="text/javascript" src="js/modernizr.custom.59319.js"></script>
+	<!-- JS -->
+
+	<!-- FAVICON -->
+	<link rel="shortcut icon" href="favicon.ico" />
+	<!-- FAVICON -->
+
+	<!--[if lt IE 9]>
+    	<script src="components/html5shiv/html5shiv.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+	
+	<section class="Contenedor__central">
+		<section class="Pre__header">
+				<ul>
+					<li><a href="#">INICIO</a></li>
+					<li><a href="#">EMPRESA</a></li>
+					<li><a href="#">ATENCION AL CLIENTE</a></li>
+					<li><a href="#">SERVICIOS</a></li>
+					<li><a href="#">MARCAS</a></li>
+					<li>C.A.B.A - Palermo - Tel: 011 4-775-0700 / Mariano Castex 2735  Tel: 4-295-7501 Canning - Ezeiza</li>
+				</ul>
+			</section> <!-- End of Pre__header -->
+		<header>
+			<section class="Logo">
+				<img src="img/logo.jpg" width="140" height="100" alt="Logo Arquinova Color">
+					<br>
+				<h1>Arquinova Color</h1>
+			</section> <!-- End of Logo -->
+		</header> <!-- End of header -->
+
+		<nav>
+			<ul>
+				<li><a href="#">REVESTIMEINTOS</a></li>
+				<li><a href="#">PINTURAS</a></li>
+				<li><a href="#">OBRAS</a></li>
+				<li><a href="#">AEROSOLES</a></li>
+				<li><a href="#">PIEDRAS</a></li>
+				<li><a href="#">PRODUCTOS</a></li>
+				<li><a href="#">ACCESORIOS</a></li>
+			</ul>
+		</nav>
+
+		<section class="Slider"></section> <!-- Slider -->
+
+		<section class="Promociones">
+			<div class="Col__map1">
+				<ul>
+					<li><img src="img/mapa1.png" alt=""></li>
+					<li><h2><a href="">CONOCE NUESTRAS <br>SUCURSALES</a></h2></li>
+				</ul>
+			</div> <!-- End of Col__map1 -->
+
+			<div class="Col__map2">
+				<ul>
+					<li><img src="img/truck.png" alt=""></li>
+					<li><h2><a href="">ENVIOS A TODO <br>EL PAIS</a></h2></li>
+				</ul>
+			</div> <!-- End of Col__map2 -->
+
+			<div class="Col__map3">
+				<ul>
+					<li><img src="img/credit.png" alt=""></li>
+					<li><h2><a href="">BENEFICIOS CON <br>TARJETAS</a></h2></li>
+				</ul>
+			</div> <!-- End of Col__map3 -->
+		</section> <!-- End of Promociones -->
+
+
+		<section class="Ml">
+			<div class="Col_Ml-1">
+				<img src="img/mercado.png" alt="">
+			</div> <!-- End of Col_Ml-1 -->
+
+			<div class="Col_Ml-2">
+				<h2>Conoc&eacute; los beneficios con Mercado Pago</h2>
+				<br>
+				<a href="http://listado.mercadolibre.com.ar/_CustId_4439710" target="_blank">INGRESAR</a>
+			</div> <!-- End of Col_Ml-2 -->
+		</section> <!-- End of Ml -->
+
+		<div class="Titulos">
+			<h2>Productos m&aacute;s vendidos</h2>
+		</div> <!-- End of Titulos -->
+
+		<section class="Destacados">
+			<div class="Col__1">
+				<div class="Emboltorio">
+					<img src="img/italflex.jpg" width="155" height="240" alt="Revestimeinto Plastico Anclaflex">
+					<h2>Revestimiento Pl&aacute;stico Anclaflex X 30 Kg</h2>
+					<p><strong>AR$670,90</strong></p>
+					<a href="http://articulo.mercadolibre.com.ar/MLA-553573406-revestimiento-plastico-tarquini-revear-anclaflex-x-30-kg-_JM" target="_blank">COMPRAR</a>
+				</div> <!-- End of Emboltorio -->
+			</div> <!-- End of Col__1 -->
+
+			<div class="Col__2">
+				<div class="Emboltorio">
+					<img src="img/revoflex.jpg" width="155" height="240" alt="Revoflex Italflex">
+					<h2>Revestimiento Pl&aacute;stico Anclaflex X 25kg</h2>
+					<p><strong>AR$850,00</strong></p>
+					<a href="http://articulo.mercadolibre.com.ar/MLA-545962796-revestimiento-plastico-revear-tarquini-anclaflex-x-25kg-_JM" target="_blank">COMPRAR</a>
+				</div> <!-- End of Emboltorio -->
+			</div> <!-- End of Col__2 -->
+
+			<div class="Col__3">
+				<div class="Emboltorio">
+					<img src="img/rus.jpg" width="155" height="240" alt="Rustiflex Anclaflex">
+					<h2>Revestimiento Pl&aacute;stico Anclaflex X 25 Kg</h2>
+					<p><strong>AR$640,90</strong></p>
+					<a href="http://articulo.mercadolibre.com.ar/MLA-551370230-revestimiento-plastico-tarquini-revear-anclaflex-x-25-kg-_JM" target="_blank">COMPRAR</a>
+				</div> <!-- End of Emboltorio -->
+			</div> <!-- End of Col__3 -->
+
+			<div class="Col__4">
+				<div class="Emboltorio">
+					<img src="img/pradera.jpg" width="155" height="240" alt="Pinturas Pradera">
+					<h2>Latex exterior impe. Color X20Lts</h2>
+					<p><strong>AR$1.120,00</strong></p>
+					<a href="http://articulo.mercadolibre.com.ar/MLA-551677448-latex-exterior-impermeabilizante-color-x-20-lts-_JM" target="_blank">COMPRAR</a>
+				</div> <!-- End of Emboltorio -->
+			</div> <!-- End of Col__4 -->
+		</section> <!-- End of Destacados -->
+
+		<div class="Titulos">
+			<h2>Algunas de nuestras marcas</h2>
+		</div> <!-- End of Titulos -->
+
+		<section class="Marcas">
+			<div class="Marcas1">
+				<img src="img/anclaflex.jpg" width="160" height="160" alt="Anclaflex">
+			</div> <!-- End of Marcas1 -->
+
+			<div class="Marcas2">
+				<img src="img/venier.jpg" width="160" height="160" alt="Venier">
+			</div> <!-- End of Marcas2 -->
+
+			<div class="Marcas3">
+				<img src="img/polacrin.jpg" width="160" height="160" alt="Polacrin">
+			</div> <!-- End of Marcas3 -->
+
+			<div class="Marcas4">
+				<img src="img/rust-oleum.jpg" width="160" height="160" alt="Rust-Oleum">
+			</div> <!-- End of Marcas4 -->
+
+			<div class="Marcas5">
+				<img src="img/steelcote.jpg" width="130" height="130" alt="Steelcote">
+			</div> <!-- End of Marcas4 -->
+
+			<div class="Marcas6">
+				<img src="img/wepel.jpg" width="160" height="160" alt="">
+			</div> <!-- End of Marcas5 -->
+		</section> <!-- End of Marcas -->
+
+		<section class="Imagenes__aleatorias"></section> <!-- End of Imagenes__aleatorias -->
+
+		<div class="Titulos">
+			<h2>Nuestras sucursales</h2>
+		</div> <!-- End of Titulos -->
+
+		<section class="Maps">
+			<div class="Mapa1">
+				<h2>Arquinova Color Suc. Canning Ezeiza</h2>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26187.54155551155!2d-58.50397826408814!3d-34.870228226455126!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd6de2c1c59bb%3A0x5ba43cd9c031c305!2sMariano+Castex%2C+Buenos+Aires!5e0!3m2!1ses!2sar!4v1428207149946" width="500" height="300" frameborder="0" style="border:0"></iframe>
+			</div> <!-- End of mapa1 -->
+
+			<div class="Mapa2">
+				<h2>Arquinova Color Suc. Palermo</h2>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13139.545247421695!2d-58.44465482435368!3d-34.58174310589094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5edd9be171d%3A0x6700691aa6a216d5!2sAv+C%C3%B3rdoba+6074%2C+Buenos+Aires%2C+Ciudad+Aut%C3%B3noma+de+Buenos+Aires!5e0!3m2!1ses!2sar!4v1428207324165" width="500" height="300" frameborder="0" style="border:0"></iframe>
+			</div> <!-- End of map2 -->
+		</section> <!-- End of Maps -->
+
+		<section class="Redes">
+			<div class="Redes1">
+				<ul>
+					<li><img src="img/facebook-logo.jpg" width="210" height="51" alt="Facebook"></li>
+					<li><a href="https://www.facebook.com/pages/Arquinova-Color/188445601354435?fref=ts" target="_blank">ENTRA AL FACEBOOK DE PALERMO</a></li>
+				</ul>
+			</div> <!-- End of Redes1 -->
+
+			<div class="Redes2">
+				<ul>
+					<li><img src="img/facebook-logo.jpg" width="210" height="51" alt="Facebook"></li>
+					<li><a href="https://www.facebook.com/pages/Arquinova-Color-Canning/511173479011749?fref=ts" target="_blank">ENTRA AL FACEBOOK DE CANNING</a></li>
+				</ul>
+			</div> <!-- End of Redes2 -->
+		</section> <!-- End of Redes -->
+
+		<footer>
+			<p>A.v. Cordoba 6074 - CABA - Tel: 011 4-775-0700</p>
+			<p>Mariano Castex 2735 - 4-295-7501 - Canning - Ezeiza</p>
+			<p>Todos los derechos reservados &copy;2014 / <a href="mailto:info@arquinovacolor.com.ar">info@arquinovacolor.com.ar</a></p>
+		</footer> <!-- End of footer -->
+
+	</section> <!-- End of Contenedor__central -->	
+
+	<?php include("includes/metricas.php"); ?>
+	<?php include("includes/olark.php"); ?>
+
+</body>
+</html>
